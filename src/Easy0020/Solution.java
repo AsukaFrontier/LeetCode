1d87370490504689bda71238a98ba878250d2371
AsukaFrontier <linkin9411@icloud.com>
2019-05-29 14:16:22 +0800

    revise;

---
+++ src/Easy0020/Solution.java
@@ -0,0 +1,14 @@
+package Easy0020;
+
+public class Solution {
+    public boolean isValid(String s)
+    {
+        //
+        return false;
+    }
+    //-----------------------------------------//
+    public static void main(String[] args)
+    {
+        //
+    }
+}
---
+++ src/Easy0026/Solution.java
@@ -0,0 +1,14 @@
+package Easy0026;
+
+public class Solution {
+    public int removeDuplicates(int[] nums)
+    {
+        //
+        return 0;
+    }
+    //---------------------------------------------//
+    public static void main(String[] args)
+    {
+        //
+    }
+}
