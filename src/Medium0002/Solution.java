0882b833ee7901739152acb349cea2d317872a78
AsukaFrontier <linkin9411@icloud.com>
2019-09-19 19:34:09 +0800

    revise;

--- src/Medium0002/Solution.java
+++ src/Medium0002/Solution.java
@@ -1,58 +1,87 @@
 package Medium0002;
-class Solution {
-    private static class ListNode
+//本方案未解决任意长度整数
+//任意长度整数可用 BigDecimal类
+import java.util.LinkedList;
+public class Solution //
+{
+    //
+    LinkedList<ListNode> listOfLN= new LinkedList<>(); //Solution类的字段
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2)
     {
-        int val;
-        ListNode next;
-        ListNode(int x)
-        {
-            val=x;
-        }
+        long num1= listToNum(l1);
+        long num2= listToNum(l2);
+        long sum= num1+ num2;
+        ListNode newHead= intToListNode(sum);
+        return newHead;
     }
-    private int addTwoNumbers(ListNode l1, ListNode l2)
+    private ListNode intToListNode(long num)
     {
-        int sum=0;
-        double pow=0;
-        ListNode l1_cur= l1;
-        ListNode l2_cur= l2;
-        while(l1_cur!=null&&l2_cur!=null)
+        if(num==0)
         {
-
-            sum=sum+(l1_cur.val+l2_cur.val)*((int)Math.pow(10.0,pow));
-            l1_cur=l1_cur.next;
-            l2_cur=l2_cur.next;
-            pow++;
+            ListNode lstZero= new ListNode(0);
+            return lstZero;
         }
-        while(l1_cur!=null)
+        //商:   quotient
+        //余数: remainder
+        while(num!=0)
         {
-            sum=sum+l1_cur.val*(int)Math.pow(10,pow);
-            l1_cur=l1_cur.next;
-            pow++;
+            long quotient= num/10;
+            long remainder= num%10;
+            ListNode current= new ListNode((int)remainder);
+            listOfLN.add(current);
+            num= quotient;
         }
-        while(l2_cur!=null)
+        ListNode head= listOfLN.removeFirst();
+        ListNode current= head;
+        while(!listOfLN.isEmpty())
         {
-            sum=sum+l2_cur.val*(int)Math.pow(10,pow);
-            l2_cur=l2_cur.next;
-            pow++;
+            current.next= listOfLN.removeFirst();
+            current= current.next;
         }
-        return sum;
+        return head;
     }
-    //--------------------------------------------//
+    private long listToNum(ListNode l1)
+    {
+        ListNode currentNode= l1;
+        long decimalOrder= 0;
+        long number= 0;
+        while(currentNode!=null)
+        {
+            number= number+currentNode.val*(long)Math.pow(10, decimalOrder);
+            decimalOrder++;
+            currentNode= currentNode.next;
+        }
+        return number;
+    }
+    //---------UNIT TEST--------------------------//
     public static void main(String[] args)
     {
-        ListNode ints1=new ListNode(2);
-        ListNode ints1_2=new ListNode(4);
-        ListNode ints1_3=new ListNode(3);
-        ints1.next=ints1_2;
-        ints1_2.next=ints1_3;
-
-        ListNode ints2=new ListNode(5);
-        ListNode ints2_2=new ListNode(6);
-        ListNode ints2_3=new ListNode(4);
-        ints2.next=ints2_2;
-        ints2_2.next=ints2_3;
-
+        //
+        ListNode l2= new ListNode(2);
+        ListNode l4= new ListNode(4);
+        ListNode l3= new ListNode(3);
+        ListNode r5= new ListNode(5);
+        ListNode r6= new ListNode(6);
+        ListNode r4= new ListNode(4);
+        l2.next= l4;
+        l4.next= l3;
+        r5.next= r6;
+        r6.next= r4;
         Solution s= new Solution();
-        System.out.println(s.addTwoNumbers(ints1,ints2));
+        ListNode lst= s.addTwoNumbers(l2, r5);
+        while(lst!=null)
+        {
+            System.out.print(lst.val+" ");
+            lst=lst.next;
+        }
+    }
+}
+class ListNode //包可见的类, 与Solution类在同一文件内
+{
+    int val;
+    ListNode next;
+    ListNode(int x)
+    {
+        val=x;
     }
 }
---
+++ src/Medium0003/Solution.java
@@ -0,0 +1,9 @@
+package Medium0003;
+//
+public class Solution //
+{
+    public int lengthOfLongestSubstring(String s)
+    {
+        return -1;
+    }
+}
